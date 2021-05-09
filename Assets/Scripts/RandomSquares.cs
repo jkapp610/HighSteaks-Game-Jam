5828cf1ac8067c73a2831e7725b167be76ddf2ed
mjspra <mjspra@umich.edu>
2021-05-08 22:43:52 -0400

    Pancake bubbles

--- Assets/Scripts/RandomSquares.cs
+++ Assets/Scripts/RandomSquares.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class RandomSquares : MonoBehaviour
 {
-    private GameObject[] circles = new GameObject[10];
-    public float timer, interval = 2f;
+    private GameObject[] circles = new GameObject[25];
+    public float timer = 2f;
+    public float interval = 0.2f;
+    public int boarder = 23;
     public Sprite circleSprite;
+    private int curr = 0;
 
     void Start()
     {
         Vector3 pos = new Vector3(-5, 0, 0);
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 25; i++)
         {
-            int randomX = Random.Range(-3, 3);
-            int randomY = Random.Range(-3, 3);
+            int randomX = Random.Range(-boarder, boarder);
+            int randomY = Random.Range(-boarder, boarder);
             int randomScale = Random.Range(1, 6);
             //circles[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             //In order to implement a sprite for this, I created a sprite object instead.
@@ -23,9 +26,10 @@ public class RandomSquares : MonoBehaviour
             SpriteRenderer newSpriteRender = circles[i].AddComponent<SpriteRenderer>();
             newSpriteRender.sprite = circleSprite;
             newSpriteRender.sortingOrder = 20; //Instead of moving the z position, we can set the sprite to display over the pan and pancake.
-            circles[i].transform.position = new Vector3(randomX, randomY, 0);
+            circles[i].transform.position = new Vector3(randomX*0.1f, randomY*0.1f, 0);
             circles[i].transform.localScale += new Vector3(randomScale*0.01f, randomScale * 0.01f, 0);
             circles[i].name = "Circle_" + i;
+            circles[i].SetActive(false);
             //pos.x++;
         }
     }
@@ -33,9 +37,13 @@ public class RandomSquares : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        if (curr < 20)
+            curr += 5;
+        else
+            curr = 0;
         if (timer >= interval)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = curr; i < curr+5; i++)
             {
                 int randomValue = Random.Range(0, 2);
                 if (randomValue == 0)
