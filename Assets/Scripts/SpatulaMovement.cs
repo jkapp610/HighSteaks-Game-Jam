f3ee8e552d8fe4af392ecf93527894bb4f88cbbd
jkapp610 <jkapp@umich.edu>
2021-05-11 08:53:26 -0400

    updated spatula script
    added  the basic  movement of the spatula moving down to the right corner and waiting when mouse is clicked.

--- Assets/Scripts/SpatulaMovement.cs
+++ Assets/Scripts/SpatulaMovement.cs
@@ -6,12 +6,19 @@ public class SpatulaMovement : MonoBehaviour{
     Vector3 mousePosition;
     public float movespeed  =0.1f;
     Rigidbody2D rigidbody;
+     public float time = 10f;
+    public float currenttime = 0;
+   public bool canmove;
+
+
     Vector2 position = new Vector2(0f,0f);
 
 
     // Start is called before the first frame update
     void Start(){
         rigidbody = GetComponent<Rigidbody2D>();
+        currenttime =time;
+        canmove = true;
     } 
 
     // Update is called once per frame
@@ -19,11 +26,42 @@ public class SpatulaMovement : MonoBehaviour{
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         position= Vector2.Lerp(transform.position,mousePosition,movespeed);
+         currenttime+= 1*Time.deltaTime;
+         if(currenttime>time){
+             canmove=true;
+         }
+
 
 
         
     }
     void FixedUpdate(){
-        rigidbody.MovePosition(position);
+       
+
+        if (canmove){
+            rigidbody.MovePosition(position);
+
+        }
+
+       
+
+         if (Input.GetMouseButtonDown(0)){
+
+          
+            
+             position = new Vector2(4.1335f,-6.459f);
+             rigidbody.MovePosition(position);
+             currenttime =0;
+             canmove =false;
+             
+                
+
+            
+            
+
+
+         }
+
+        //x 4.1335 //y -6459
     }
 }
