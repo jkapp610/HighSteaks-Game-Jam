e813c231b30164b2ae98a01ae5f72359fd7731ed
jkapp610 <jkapp@umich.edu>
2021-05-13 22:15:16 -0400

    update
    updated gravity and spatula ,adde flip meter to all the  levels and added mouse click to grill fork

--- Assets/Scripts/GrillforkMovement.cs
+++ Assets/Scripts/GrillforkMovement.cs
@@ -8,10 +8,15 @@ public class GrillforkMovement : MonoBehaviour
     public float movespeed  =0.1f;
     Rigidbody2D rigidbody;
     Vector2 position = new Vector2(0f,0f);
+    public float time = 10f;
+    public float currenttime = 0;
+   public bool canmove;
 
     // Start is called before the first frame update
     void Start(){
-          rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        currenttime =time;
+        canmove = true;
         
     }
 
@@ -20,9 +25,38 @@ public class GrillforkMovement : MonoBehaviour
          mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         position= Vector2.Lerp(transform.position,mousePosition,movespeed);
+        currenttime+= 1*Time.deltaTime;
+        if(currenttime>time){
+            canmove=true;
+         }
+
         
     }
     void FixedUpdate(){
-        rigidbody.MovePosition(position);
+        
+        if (canmove){
+            rigidbody.MovePosition(position);
+
+        }
+
+       
+
+         if (Input.GetMouseButtonDown(0)){
+
+          
+            
+             position = new Vector2(3.8166f,-4.585f);
+             rigidbody.MovePosition(position);
+             currenttime =0;
+             canmove =false;
+             
+                
+
+            
+            
+
+
+         }
+
     }
 }
