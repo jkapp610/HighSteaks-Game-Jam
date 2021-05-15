030ba623bf981598d6ddef08553bb81eb2b50a2b
nhelz <nhelz@umich.edu>
2021-05-15 18:19:25 -0400

    Updating pancake dial

--- Assets/Scripts/DialTurning.cs
+++ Assets/Scripts/DialTurning.cs
@@ -6,7 +6,8 @@ public class DialTurning : MonoBehaviour
 {
     //public GameObject mouseCursor;
     public StoveFlame flame;
-    private float rotZ;
+    [SerializeField]
+    private float rotZ = 359f;
     public GameObject dial;
     public RandomBubbles bubbles;
     public ScoringPancake score;
@@ -36,57 +37,66 @@ public class DialTurning : MonoBehaviour
         randInt = Random.Range(0, 10000);
 
         Zval = dial.transform.rotation.z;
-         ZvaleulerAngle = dial.transform.localRotation.eulerAngles.z;
+        ZvaleulerAngle = dial.transform.localRotation.eulerAngles.z;
 ;
 
 
         if (randInt <= decayGoal) {
             decaying = true;
         }
-
+        /*
         if(ZvaleulerAngle <= 181||Input.GetKeyUp("d")||Input.GetKeyDown("a")){
             //clockwise = false;
          
         }
-    
+        */
         if(Zval>=0||Input.GetKeyUp("a")||Input.GetKeyDown("d")){
             //clockwise = true;
             decaying = false;
         }
 
 
-      
+        rotZ += Input.GetAxis("Horizontal") * -1;
+        rotZ = Mathf.Clamp(rotZ, 180, 360);
        // if(clockwise ==true){
-            if(Input.GetKey(KeyCode.D)){
-                rotZ += -Time.deltaTime*speed;
+            if(Input.GetKeyDown(KeyCode.D)){
+                //rotZ -= 1f;
+            /*
                 dial.transform.rotation = Quaternion.Euler(0,0,rotZ);
                 flame.SetFlameViaZVal(Zval);
                 bubbles.SetTempViaZVal(Zval);
-                score.SetHeatIncrementViaZVal(Zval);
+                score.SetHeatIncrementViaZVal(Zval);*/
             } 
         //}
 
 
         //if(clockwise ==false){
-            if(Input.GetKey(KeyCode.A)){
-                rotZ += Time.deltaTime*speed;
+            if(Input.GetKeyDown(KeyCode.A)){
+                //rotZ += 1f;
+            /*
                 dial.transform.rotation = Quaternion.Euler(0,0,rotZ);
                 flame.SetFlameViaZVal(Zval);
                 bubbles.SetTempViaZVal(Zval);
-                score.SetHeatIncrementViaZVal(Zval);
-            }   
+                score.SetHeatIncrementViaZVal(Zval);*/
+            }
 
-       // }
+        // }
 
-        if(decaying && Zval < 0) {
+        dial.transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        
+
+
+        /*
+        if (decaying && Zval < 0) {
             rotZ += Time.deltaTime * speed / decaySpdDivisor;
+            
             dial.transform.rotation = Quaternion.Euler(0,0,rotZ);
             flame.SetFlameViaZVal(Zval);
             bubbles.SetTempViaZVal(Zval);
             score.SetHeatIncrementViaZVal(Zval);
-        }
+         }
+    */
 
-    
         //cursorPosition = Input.mousePosition;
 
     }
